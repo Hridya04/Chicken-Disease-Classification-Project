c8a90a6493f19b32e12e659f73d4a255b8d02bec
Hridya04 <hridyaramesh99@gmail.com>
2026-08-19 13:02:43 +0530

    data ingestion completed

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -13,18 +13,19 @@ SRC_REPO = "CNNClassifier"
 AUTHOR_EMAIL = "hridyaramesh99@gmail.com"
 
 
-setuptools.setup(
+setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
-    Long_description_content="text/markdown",
+    long_description=long_description,
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
 
     },
-    package_dir={"", "src"},
-    packages=setuptools.find_packages(where="src")
+    package_dir={"": "src"},
+    packages=find_packages(where="src")
 )
--- template.py
+++ template.py
@@ -10,8 +10,8 @@ project_name="CNNClassifier"
 
 
 list_of_files=[
-    ",github/workflows/.gitkeep",
-    f"src/{project_name}/__init__'py",
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
